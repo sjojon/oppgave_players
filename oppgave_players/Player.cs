ab2605c27d1092e864f98c4e11422ae6eb35f0dd
sjojon <90796535+sjojon@users.noreply.github.com>
2023-05-28 12:38:59 +0200

    Initial commit

---
+++ oppgave_players/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Linq;
+
+namespace oppgave_players
+{
+    internal class Player
+    {
+        public string Name { get; private set; }
+        public int Points { get; private set; }
+
+        public Player(string name, int points)
+        {
+            Name = name;
+            Points = points;
+        }
+
+        public int Play(Player player)
+        {
+            var random = new Random();
+            var number = random.Next(0, 2);
+            if (number == 1)
+            {
+                player.Points++;
+            }
+            else
+            {
+                player.Points--;
+            }
+
+            return player.Points;
+        }
+
+        public static void ShowNameAndPoints(Player player)
+        {
+            Console.WriteLine($"Name: {player.Name}\nPoints: {player.Points}");
+            Console.WriteLine();
+        }
+
+        public string GetMatchInfo(Player opponent)
+        {
+            //string result = $"{Name} Vs. {opponent.Name}: {Points} -  {opponent.Points}";
+            //return result;
+
+            int scoreDifference = Points - opponent.Points;
+            string result = $"{Name} vs. {opponent.Name}: Score difference: {scoreDifference}";
+            return result;
+        }
+    }
+}
---
+++ oppgave_players/Program.cs
@@ -0,0 +1,33 @@
+﻿using oppgave_players;
+
+var random = new Random();
+var players = new[]
+{
+    new Player("Per", 10),
+    new Player("Pål", 10),
+    new Player("Espen", 10)
+};
+
+for (var round = 1; round <= 10; round++)
+{
+    var playerIndex1 = random.Next(players.Length);
+    var playerIndex2 = (playerIndex1 + 1 + random.Next(2)) % players.Length;
+    var player1 = players[playerIndex1];
+    var player2 = players[playerIndex2];
+    player1.Play(player2);
+}
+
+for (int i = 0; i < players.Length; i++)
+{
+    for (int j = 0; j < players.Length; j++)
+    {
+        string matchInfo = players[i].GetMatchInfo(players[j]);
+        Console.WriteLine(matchInfo);
+    }
+}
+
+Console.WriteLine();
+foreach (var player in players)
+{
+    Player.ShowNameAndPoints(player);
+}
